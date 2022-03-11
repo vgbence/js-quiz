e237d98fbd23d98dd52cb370d89ede0442dc3590
vgbence <vgbence@yahoo.com>
2022-03-11 17:28:22 +0100

    plus css and bounce

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const correctAnswers = ["B", "C", "A", "C", "C" ]
+const correctAnswers = ["B", "C", "A", "C", "C"]
 const form = document.querySelector(".quiz-form")
 const result = document.querySelector(".result")
 const span = result.querySelector("span")
@@ -11,26 +11,26 @@ form.addEventListener("submit", e => {
     userAnswers.forEach((answer, index) => {
         if (answer === correctAnswers[index]) score += 20;
     })
-    scrollTo(0,0)
-    
+    scrollTo(0, 0)
+
     result.classList.remove("d-none")
-    
+
     let output = 0;
     const timer = setInterval(() => {
         result.querySelector("span").textContent = `${output}%`;
-        if(output === 0)
-        h2.textContent = "Let's try again, shall we?"
-        else if(output === 20)
-        h2.textContent = "Don't give up, let's give it another try!"
-        else if(output === 40)
-        h2.textContent = "Not great, not terrible..."
-        else if(output === 60)
-        h2.textContent = "You are on the right track, keep it up!"
-        else if(output === 80)
-        h2.textContent = "You only missed one question, good job!"
-        else if(output === 100)
-        h2.textContent = "Congratulations, you have answered all the questions correctly"
-        if(output === score) {
+        if (output === 0)
+            h2.textContent = "Let's try again, shall we?"
+        else if (output === 20)
+            h2.textContent = "Don't give up, let's give it another try!"
+        else if (output === 40)
+            h2.textContent = "Not great, not terrible..."
+        else if (output === 60)
+            h2.textContent = "You are on the right track, keep it up!"
+        else if (output === 80)
+            h2.textContent = "You only missed one question, good job!"
+        else if (output === 100)
+            h2.textContent = "Congratulations!"
+        if (output === score) {
             clearInterval(timer)
         } else {
             output++
